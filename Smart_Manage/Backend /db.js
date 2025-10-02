c063398e2335c4e667c0714714e9ab6a9bb1bc99
Aronjohn20 <aronjohn12112004@gmail.com>
2025-10-03 01:45:07 +0530

    Update db.js

--- Smart_Manage/Backend /db.js
+++ Smart_Manage/Backend /db.js
@@ -1,1 +1,16 @@
+// backend/db.js
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connect;
