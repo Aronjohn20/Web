4091c3d1bbc49d6ef4044e2e4eed79884de916b1
Aronjohn20 <aronjohn12112004@gmail.com>
2025-10-03 01:51:46 +0530

    Update index.js

--- Smart_Manage/frontend/src/index.js
+++ Smart_Manage/frontend/src/index.js
@@ -1,1 +1,8 @@
+// frontend/src/index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
